99888f1d6b0702b474aa7a81196ee90ff37fc5df
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-09-06 20:47:24 -0600

    add: moongose config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,8 @@
-import express, { Express } from "express";
-import { PORT } from "../config";
-import router from "../routes/videos.routes";
+import app from "./app";
+import { PORT } from "./config";
+import { connectDB } from "./db";
 
-const app: Express = express();
-
-app.use(express.json());
-
-app.use(router);
+connectDB()
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
