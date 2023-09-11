b3ede816c27e8398fd7edc154e36624a59e1f432
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-09-10 20:22:29 -0600

    add: get all videos

--- client/src/pages/videos/VideosPage.tsx
+++ client/src/pages/videos/VideosPage.tsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+type Video = {
+  _id: string; 
+  name: string;
+  tempFilePath: string;
+};
+
 function VideosPage() {
+  const [videos, setVideos] = useState<Video[]>([]);
+
+  useEffect(() => {
+    const getAllVideos = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/");
+        setVideos(response.data);
+      } catch (error) {
+        console.error("Error al obtener videos:", error);
+      }
+    };
+
+    getAllVideos();
+  }, []);
   return (
-    <div>All videos</div>
-  )
+    <div>
+      <h1>Lista de Videos</h1>
+      <ul>
+        {videos.map(video => (
+          <li key={video._id}>
+            <video controls>
+              <source src={`http://localhost:3000/${video.tempFilePath}`} type="video/mp4" />
+            </video>
+            <p>{video.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default VideosPage
+export default VideosPage;
