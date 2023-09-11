3359961d66b1f107df10af206ad71ece435b2b87
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-09-10 20:22:16 -0600

    fixed: upload videos error handler

--- client/src/pages/videos/UploadVideoPage.tsx
+++ client/src/pages/videos/UploadVideoPage.tsx
@@ -2,31 +2,37 @@ import axios from "axios";
 import { useState } from "react";
 
 function UploadVideoPage() {
-    const [file, setFile] = useState<any>({
-        name: "",
-      });
-    
-      const [isFileSelected, setFileSelected] = useState<boolean>(false);
-    
-      const handlerSubmit = async () => {
-        const formData = new FormData();
-        formData.append("file", file.name!);
-        await axios.post("http://localhost:3000/upload", formData);
-        console.log("Video uploaded successfully");
-      };
-    
-      const handlerInput = (e: any) => {
-        setFile({ ...file, name: e.target.files![0] });
-        setFileSelected(true);
-      };
+  const [file, setFile] = useState<any>({
+    name: "",
+  });
+
+  const [isFileSelected, setFileSelected] = useState<boolean>(false);
+
+  const handlerSubmit = async (e:any) => {
+    e.preventDefault()
+    try {
+      const formData = new FormData();
+      formData.append("file", file.name!);
+      console.log(file.name)
+      await axios.post("http://localhost:3000/upload", formData);
+      console.log("Video uploaded successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handlerInput = (e: any) => {
+    setFile({ ...file, name: e.target.files![0] });
+    setFileSelected(true);
+  };
   return (
     <form onSubmit={handlerSubmit}>
-        <input type="file" onChange={handlerInput} accept="video/*" />
-        <button disabled={!isFileSelected} type="submit">
-          Submit Video
-        </button>
-      </form>
-  )
+      <input type="file" onChange={handlerInput} accept="video/*" />
+      <button disabled={!isFileSelected} type="submit">
+        Submit Video
+      </button>
+    </form>
+  );
 }
 
-export default UploadVideoPage
+export default UploadVideoPage;
