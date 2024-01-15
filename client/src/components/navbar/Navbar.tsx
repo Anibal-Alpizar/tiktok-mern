b2d5ba22eaa3983021944e7af13216ae2135cbab
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-14 22:47:02 -0600

    Update Navbar component styling and add new navigation options

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,17 +1,41 @@
 import { Link } from "react-router-dom";
-import { navigation } from "./navigation";
+import { navigations, mainNavigation as logo } from "./navigation";
 
 function Navbar() {
   return (
-    <nav className="flex justify-between bg-gray-950 text-white py-5 px-10">
-      {navigation.map(({ href, name, icon: Icon }, i) => {
+    <nav
+      className="flex justify-between text-white py-5 px-10"
+      style={{ backgroundColor: "#121212", borderBottom: "5px solid #151515" }}
+    >
+      {logo.map(({ href, name, icon: Icon }, i) => {
         return (
-          <Link to={href} key={i}>
+          <Link className="flex items-center gap-x-2" to={href} key={i}>
             <Icon />
-            <h1>{name}</h1>
+            <h1 className="font-bold text-3xl">{name}</h1>
           </Link>
         );
       })}
+      <div className="flex gap-x-5">
+        {navigations.map(({ href, name, icon: Icon }, i) => {
+          return (
+            <Link className="flex items-center gap-x-2" to={href} key={i}>
+              <h1
+                className={`flex justify-center items-center gap-x-2 font-bold ${
+                  name === "Upload" ? "custom-upload-style" : ""
+                }`}
+                style={
+                  name === "Upload"
+                    ? { backgroundColor: "#1B1B1B", padding: "10px" }
+                    : {}
+                }
+              >
+                <Icon />
+                {name}
+              </h1>
+            </Link>
+          );
+        })}
+      </div>
     </nav>
   );
 }
