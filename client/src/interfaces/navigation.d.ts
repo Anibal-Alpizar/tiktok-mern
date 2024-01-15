cbb0602af5a3706bb7b48960721785bbde14db27
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-14 22:47:13 -0600

    Update NavigationItem interface to NavigationsItem and add MainNavigation interface

--- client/src/interfaces/navigation.d.ts
+++ client/src/interfaces/navigation.d.ts
@@ -1,4 +1,10 @@
-export interface NavigationItem {
+export interface NavigationsItem {
+  name: string;
+  href: string;
+  icon: React.ComponentType;
+}
+
+export interface MainNavigation {
   name: string;
   href: string;
   icon: React.ComponentType;
