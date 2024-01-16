bee379f81ca452ebae93a9ca588bb87c6a75c2cb
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-15 23:13:00 -0600

    Add loadVideos function and videos state to VideosProvider

--- client/src/context/provider/VideosProvider.tsx
+++ client/src/context/provider/VideosProvider.tsx
@@ -1,8 +1,24 @@
 import { VideosContext } from "../VideosContext";
-import { uploadVideoRequest } from "../../api/video.api";
-import { File } from "../../interfaces/video";
+import { getVideosRequest, uploadVideoRequest } from "../../api/video.api";
+import { File, Video } from "../../interfaces/video";
+import { useState } from "react";
 
 export const VideoProvider = ({ children }: any) => {
+  const [videos, setVideos] = useState<Video[]>([]);
+
+  const loadVideos = async () => {
+    try {
+      const response = await getVideosRequest();
+      const modifiedVideos = response.data.map((video: Video) => ({
+        ...video,
+        tempFilePath: video.tempFilePath.replace("videos\\", ""),
+      }));
+
+      setVideos(modifiedVideos);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const uploadVideo = async (file: File) => {
     try {
@@ -18,6 +34,8 @@ export const VideoProvider = ({ children }: any) => {
     <VideosContext.Provider
       value={{
         uploadVideo,
+        loadVideos,
+        videos,
       }}
     >
       {children}
