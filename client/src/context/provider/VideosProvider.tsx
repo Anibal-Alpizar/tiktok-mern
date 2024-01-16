84cd426ec815ff983489884e251af189e5037671
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-15 22:43:44 -0600

    Add uploadVideo function to VideosProvider

--- client/src/context/provider/VideosProvider.tsx
+++ client/src/context/provider/VideosProvider.tsx
@@ -1,10 +1,23 @@
 import { VideosContext } from "../VideosContext";
+import { uploadVideoRequest } from "../../api/video.api";
+import { File } from "../../interfaces/video";
 
 export const VideoProvider = ({ children }: any) => {
+
+  const uploadVideo = async (file: File) => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file.name!);
+      const response = await uploadVideoRequest(formData);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <VideosContext.Provider
       value={{
-        hello: "world",
+        uploadVideo,
       }}
     >
       {children}
