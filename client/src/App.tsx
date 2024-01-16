23f0ff7c7003e655b1323b4baeb8a1a1b4623ea1
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-15 19:49:57 -0600

    Refactor routing in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
-import { Route, Routes } from "react-router-dom";
-import Navbar  from "./components/navbar/Navbar"
+import { Outlet, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
 import VideosPage from "./pages/videos/VideosPage";
 import UploadVideoPage from "./pages/videos/UploadVideoPage";
+import { VideoProvider } from "./context/provider/VideosProvider";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
@@ -9,8 +10,16 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<VideosPage />} />
-        <Route path="/new" element={<UploadVideoPage />} />
+        <Route
+          element={
+            <VideoProvider>
+              <Outlet />
+            </VideoProvider>
+          }
+        >
+          <Route path="/" element={<VideosPage />} />
+          <Route path="/new" element={<UploadVideoPage />} />
+        </Route>
       </Routes>
     </>
   );
