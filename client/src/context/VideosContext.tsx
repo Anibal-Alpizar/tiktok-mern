8a778154b54f91430e968666f2acf8bb2e5486fe
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-15 23:12:45 -0600

    Add loadVideos function and update VideosContext interface

--- client/src/context/VideosContext.tsx
+++ client/src/context/VideosContext.tsx
@@ -1,8 +1,10 @@
 import { createContext } from "react";
-import { File } from "../interfaces/video";
+import { File, Video } from "../interfaces/video";
 
 interface VideosContextProps {
   uploadVideo: (file: File) => void;
+  loadVideos: () => void;
+  videos: Video[];
 }
 
 export const VideosContext = createContext<VideosContextProps | undefined>(
