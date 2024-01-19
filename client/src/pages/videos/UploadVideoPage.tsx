3dc3a565381d5fb634b9ec486e544339f981f331
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-19 17:14:06 -0600

    refactor: Update UploadVideoPage component
    - Replaced the usage of `FormEvent` and manual form handling with `react-hook-form` for better form management
    - Added a text input for video description using `react-hook-form` for improved user interaction
    - Updated the form submission logic to include the video description when calling the 'uploadVideo' function
    - Improved code organization and readability

--- client/src/pages/videos/UploadVideoPage.tsx
+++ client/src/pages/videos/UploadVideoPage.tsx
@@ -1,25 +1,29 @@
-import { useState, FormEvent } from "react";
+import { useState } from "react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { useVideos } from "../../hooks/useVideos";
 import { File } from "../../interfaces/video";
 import FileInput from "../../components/ui/FileInput";
+import { useForm } from "react-hook-form";
 
 function UploadVideoPage() {
   const navigate = useNavigate();
   const { uploadVideo } = useVideos();
 
+  const { register, handleSubmit } = useForm();
   const [file, setFile] = useState<File>();
   const [isFileSelected, setFileSelected] = useState<boolean>(false);
 
-  const handlerSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const onSubmit = async (data: any) => {
     try {
+      console.log(data.description);
       if (file) {
-        await uploadVideo(file);
+        await uploadVideo(file, data.description);
         toast.success("Video uploaded successfully");
         navigate("/");
-      } else toast.error("Please select a video");
+      } else {
+        toast.error("Please select a video");
+      }
     } catch (error) {
       toast.error("Error uploading video");
       console.log(error);
@@ -33,10 +37,16 @@ function UploadVideoPage() {
 
   return (
     <div className="max-w-7xl px-4 mx-auto">
-      <form onSubmit={handlerSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <button disabled={!isFileSelected} type="submit">
           Submit Video
         </button>
+        <input
+          autoFocus
+          {...register("description")}
+          type="text"
+          placeholder="Description video"
+        />
         <FileInput onchange={handlerInput} />
       </form>
     </div>
