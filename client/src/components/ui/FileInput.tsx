f0b8655d5c0b9dae7e45f42286a8b1fe0391a093
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-19 17:12:53 -0600

    style: Standardize attribute naming in FileInput component
    - Updated the attribute naming convention from camelCase to kebab-case for consistency
    - Replaced 'strokeWidth' with 'stroke-width' and 'strokeLinecap' with 'stroke-linecap'
    - Ensured that all attributes adhere to the kebab-case naming convention
    - Improved code readability and maintained styling consistency

--- client/src/components/ui/FileInput.tsx
+++ client/src/components/ui/FileInput.tsx
@@ -1,8 +1,4 @@
-
-
-
-
-function FileInput({ onchange}:any) {
+function FileInput({ onchange }: any) {
   return (
     <div className="flex items-center justify-center w-full">
       <label className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
@@ -16,9 +12,9 @@ function FileInput({ onchange}:any) {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
             />
           </svg>
@@ -40,8 +36,12 @@ function FileInput({ onchange}:any) {
         >
           Select file
         </i>
-        <input id="dropzone-file" type="file" className="hidden" onChange={onchange} />
-        
+        <input
+          id="dropzone-file"
+          type="file"
+          className="hidden"
+          onChange={onchange}
+        />
       </label>
     </div>
   );
