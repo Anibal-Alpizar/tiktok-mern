f5c0d3d5fe252f7c91ce8fde507ca9f67c0a6af9
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-21 16:23:30 -0600

    feat: implement user registration in auth.controller
    Added a new file auth.controller.ts with a function to handle user registration. The function utilizes the Auth model to create a new user with provided name, email, and password. Any errors during the registration process are logged and rethrown.

---
+++ server/src/helpers/auth.helper.ts
@@ -0,0 +1,22 @@
+import Auth from "../models/auth.model";
+
+const registerUser = async (name: string, email: string, password: string) => {
+  try {
+    const newUser = new Auth({
+      name,
+      email,
+      password,
+    });
+
+    await newUser.save();
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const authActions = {
+  registerUser,
+};
