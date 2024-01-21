743f0613ea0b83ed6fce064e23471222062bf4db
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-21 16:21:48 -0600

    refactor: reorganize route imports in app.ts
    Updated the app.ts file to reorganize route imports for better code organization. Separated video-related routes and authentication routes into separate imports for clarity and maintainability.

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,8 @@ import express, { Express } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import fileUpload from "express-fileupload";
-import router from "./routes/videos.routes";
+import videosRouter from "./routes/videos.routes";
+import authRouter from "./routes/auth.routes";
 
 const app: Express = express();
 
@@ -21,9 +22,10 @@ app.use(
 
 app.use(morgan("dev"));
 app.use(express.json());
- 
+
 app.use("/videos", express.static("videos"));
 
-app.use(router);
+app.use(videosRouter);
+app.use(authRouter);
 
 export default app;
