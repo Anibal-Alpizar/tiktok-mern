9d4ed16529fcf86653fb06e0b71feab826899868
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-21 18:55:36 -0600

    refactor: Improve user registration handling in Express app

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -8,10 +8,19 @@ export const signup = async (
 ) => {
   const { name, email, password } = req.body;
   try {
+    const existingUser = await authActions.getUserByEmail(email);
+
+    if (existingUser)
+      return res
+        .status(400)
+        .json({ error: "User with this email already exists" });
+
     const newUser = await authActions.registerUser(name, email, password);
     res.status(200).json({ message: newUser });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: "Error while registering user" });
+    next(error);
   }
 };
 
