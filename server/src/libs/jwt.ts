705db1b81b53762ed5f0c81f13eef1b3cc6c0476
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-22 14:21:47 -0600

    style: Update token expiration format in createAccessToken function

--- server/src/libs/jwt.ts
+++ server/src/libs/jwt.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const createAccessToken = (payload: object) => {
   return new Promise((resolve, reject) => {
-    jwt.sign(payload, "change", { expiresIn: `1d` }, (err, token) => {
+    jwt.sign(payload, "change", { expiresIn: '1d' }, (err, token) => {
       if (err) reject(err);
       resolve(token);
     });
