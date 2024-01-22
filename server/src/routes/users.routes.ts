59cdfda7b9a90cb58d47077d5aac98e4dd2d559a
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-22 14:23:05 -0600

    refactor: Add isAuth middleware to user routes
    - Protect the "/users" route with the isAuth middleware.

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,9 +1,10 @@
-import { Router } from "express";
+import { Router, RequestHandler } from "express";
 import { getAllUsers, deleteAllUsers } from "../controllers/users.controller";
+import { isAuth } from "../middleware/auth.middleware";
 
 const router: Router = Router();
 
-router.get("/users", getAllUsers);
+router.get("/users", [isAuth as RequestHandler], getAllUsers);
 
 router.delete("/users", deleteAllUsers);
 
