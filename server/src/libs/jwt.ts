eafed9959640c73f1e6ef13a7878b86f05dcfe13
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-22 14:52:31 -0600

    chore: Use environment variable for JWT secret
    - Import `Secret` from "jsonwebtoken".
    - Replace the hardcoded string "change" with `process.env.JWT_SECRET` for the JWT secret.
    - Update the JWT sign function accordingly.

--- server/src/libs/jwt.ts
+++ server/src/libs/jwt.ts
@@ -1,10 +1,15 @@
-import jwt from "jsonwebtoken";
+import jwt, { Secret } from "jsonwebtoken";
 
 export const createAccessToken = (payload: object) => {
   return new Promise((resolve, reject) => {
-    jwt.sign(payload, "change", { expiresIn: '1d' }, (err, token) => {
-      if (err) reject(err);
-      resolve(token);
-    });
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET as Secret,
+      { expiresIn: "1d" },
+      (err, token) => {
+        if (err) reject(err);
+        resolve(token);
+      }
+    );
   });
 };
