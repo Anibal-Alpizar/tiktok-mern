8f488f881cd21f72d06764f1d0f17aadde2da852
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-21 19:10:44 -0600

    feat: Add function to create JWT access token
    - Create JWT access token with a 1-day expiration.

---
+++ server/src/libs/jwt.ts
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken";
+
+export const createAccessToken = (payload: object) => {
+  return new Promise((resolve, reject) => {
+    jwt.sign(payload, "change", { expiresIn: `1d` }, (err, token) => {
+      if (err) reject(err);
+      resolve(token);
+    });
+  });
+};
