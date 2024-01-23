f08c786158cd93f6ca776b3d9c531c2c3cfe0599
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-23 00:57:45 -0600

    feat: Add request validation for signup route
    - Integrate request validation middleware for the 'signup' route.
    - Import the necessary validation middleware and schema for signup.
    - Apply the middleware to the 'signup' route.

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,10 +1,12 @@
 import { Router, RequestHandler } from "express";
 import { signup, signin, signout, me } from "../controllers/auth.controller";
 import { isAuth } from "../middleware/auth.middleware";
+import { validateSchema } from "../middleware/validate.middleware";
+import { signupSchema } from "../schema/auth.schema";
 
 const router: Router = Router();
 
-router.post("/signup", signup);
+router.post("/signup", [validateSchema(signupSchema)], signup);
 
 router.post("/signin", signin);
 
