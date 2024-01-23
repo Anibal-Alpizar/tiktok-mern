a8c3cdb56c876423edef5daddb46bcd20330140c
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-23 00:28:37 -0600

    feat: Add '/me' route to retrieve user information
    - Import the 'me' function from the 'auth.controller'.
    - Add a new route '/me' that uses the 'isAuth' middleware to check if the user is authenticated.
    - If authenticated, invoke the 'me' function to retrieve user information.

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,5 +1,6 @@
-import { Router } from "express";
-import { signup, signin, signout } from "../controllers/auth.controller";
+import { Router, RequestHandler } from "express";
+import { signup, signin, signout, me } from "../controllers/auth.controller";
+import { isAuth } from "../middleware/auth.middleware";
 
 const router: Router = Router();
 
@@ -8,5 +9,7 @@ router.post("/signup", signup);
 router.post("/signin", signin);
 
 router.get("/signout", signout);
+
+router.get("/me", [isAuth as RequestHandler], me);
 
 export default router;
