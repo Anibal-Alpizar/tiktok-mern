8eeae6caf442aede82be4c4beeb4849f991f8ac6
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-24 01:55:42 -0600

    chore: Refactor Video component
    - Remove the unnecessary <ul> and <FaRegUserCircle> elements.
    - Adjust the structure to wrap each video item in a <div> with a flex display.

--- client/src/components/videos/Video.tsx
+++ client/src/components/videos/Video.tsx
@@ -9,35 +9,40 @@ interface VideoProps {
 
 export function Video({ videos }: VideoProps) {
   return (
-    <ul className="flex" style={{ flexWrap: "inherit" }}>
-      <FaRegUserCircle className="text-4xl text-white" />
+    <ul className="" style={{ flexWrap: "inherit" }}>
       {videos.map((video: VideoType) => (
         <li key={video._id} className="mb-3">
-          <div className="rounded-md" style={{ width: "80%", margin: "auto" }}>
-            <h1 className="font-bold text-lg">John Doe</h1>
-            <p className="text-white text-lg">{video.description}</p>
-            <div className="flex items-center gap-x-2 mb-3">
-              <IoIosMusicalNotes />
-              <h2 className="text-white text-sm">{video.name}</h2>
-            </div>
-            <video
-              controls
-              autoPlay={true}
-              muted
-              loop
-              style={{
-                borderRadius: "8px",
-                width: "22rem",
-                height: "490px",
-                objectFit: "cover",
-              }}
+          <div className="flex">
+            <FaRegUserCircle className="text-4xl text-white" />
+            <div
+              className="rounded-md"
+              style={{ width: "80%", margin: "auto" }}
             >
-              <source
-                src={`${HTTP.API_URL_VIDEOS}/${video.tempFilePath}`}
-                type="video/mp4"
-              />
-              {ERRORS.VIDEO_NOT_SUPPORTED}
-            </video>
+              <h1 className="font-bold text-lg">John Doe</h1>
+              <p className="text-white text-lg">{video.description}</p>
+              <div className="flex items-center gap-x-2 mb-3">
+                <IoIosMusicalNotes />
+                <h2 className="text-white text-sm">{video.name}</h2>
+              </div>
+              <video
+                controls
+                autoPlay={true}
+                muted
+                loop
+                style={{
+                  borderRadius: "8px",
+                  width: "22rem",
+                  height: "490px",
+                  objectFit: "cover",
+                }}
+              >
+                <source
+                  src={`${HTTP.API_URL_VIDEOS}/${video.tempFilePath}`}
+                  type="video/mp4"
+                />
+                {ERRORS.VIDEO_NOT_SUPPORTED}
+              </video>
+            </div>
           </div>
         </li>
       ))}
