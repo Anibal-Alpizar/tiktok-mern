8c8da1fee4241001a3080df8de4ae86923eab5b5
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-25 19:32:12 -0600

    Add Video and VideoProps interfaces

---
+++ client/src/interfaces/video/videoProps.ts
@@ -0,0 +1,17 @@
+export interface Video {
+  _id: string;
+  name: string;
+  tempFilePath: string;
+  description: string;
+}
+
+export interface VideoProps {
+  videos: Video[];
+}
+
+export interface Video {
+  _id: string;
+  name: string;
+  tempFilePath: string;
+  description: string;
+}
