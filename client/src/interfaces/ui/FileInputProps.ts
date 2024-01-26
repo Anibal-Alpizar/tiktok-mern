4ec69ac7c196822d0673c0194ee05ad254f09bd9
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-25 22:15:57 -0600

    Fix typo in FileInputProps interface

--- client/src/interfaces/ui/FileInputProps.ts
+++ client/src/interfaces/ui/FileInputProps.ts
@@ -1,5 +1,5 @@
 import { ChangeEvent } from "react";
 
-export interface FileInputProps {
-  onchange: (event: ChangeEvent<HTMLInputElement>) => void;
+export interface fileInputProps {
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
