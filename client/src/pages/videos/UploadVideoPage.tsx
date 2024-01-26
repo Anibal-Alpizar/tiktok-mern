599cc57100bde8a16add0b571c103d63be874f6d
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-25 22:15:35 -0600

    Update imports and components in UploadVideoPage

--- client/src/pages/videos/UploadVideoPage.tsx
+++ client/src/pages/videos/UploadVideoPage.tsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { useVideos } from "../../hooks/useVideos";
-import { File } from "../../interfaces/video";
-import { FileInput } from "../../components/ui";
+import { file } from "../../interfaces/video/videoProps";
+import { Button, FileInput, Input } from "../../components/ui";
 import { useForm } from "react-hook-form";
 
 function UploadVideoPage() {
@@ -11,7 +11,7 @@ function UploadVideoPage() {
   const { uploadVideo } = useVideos();
 
   const { register, handleSubmit } = useForm();
-  const [file, setFile] = useState<File>();
+  const [file, setFile] = useState<file>();
   const [isFileSelected, setFileSelected] = useState<boolean>(false);
 
   const onSubmit = async (data: any) => {
@@ -38,17 +38,21 @@ function UploadVideoPage() {
   return (
     <div className="max-w-7xl px-4 mx-auto">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <button disabled={!isFileSelected} type="submit">
-          Submit Video
-        </button>
-        <input
-          className="text-black"
+        <Input
+          className="text-black w-full mb-4 text-base px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
           autoFocus
           {...register("description")}
           type="text"
           placeholder="Description video"
         />
-        <FileInput onchange={handlerInput} />
+        <FileInput onChange={handlerInput} />
+        <Button
+          className="w-full mt-4 text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg"
+          disabled={!isFileSelected}
+          type="submit"
+        >
+          Submit Video
+        </Button>
       </form>
     </div>
   );
