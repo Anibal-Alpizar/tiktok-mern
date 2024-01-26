0baf7f097639357c0b67daf04eadda3094c417cf
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-25 22:16:03 -0600

    Update ButtonProps interface

--- client/src/interfaces/ui/ButtonProps.ts
+++ client/src/interfaces/ui/ButtonProps.ts
@@ -1,5 +1,7 @@
-export interface ButtonProps {
+export interface buttonProps {
   children: React.ReactNode;
   type?: "button" | "submit" | "reset";
   onClick?: () => void;
+  disabled?: boolean;
+  className?: string;
 }
