d72543024a425ef898d6ed5a35ebf07023bdf507
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-25 22:16:23 -0600

    Refactor Input component to use forwardRef and add additional props

--- client/src/components/ui/Input.tsx
+++ client/src/components/ui/Input.tsx
@@ -1,3 +1,17 @@
-export function Input({ type }: { type: string }) {
-  return <input type={type} />;
-}
+import { forwardRef } from "react";
+import { inputProps } from "../../interfaces/ui/inputProps";
+
+export const Input = forwardRef<HTMLInputElement, inputProps>(
+  ({ type, placeholder, className, autoFocus, ...rest }, ref) => {
+    return (
+      <input
+        type={type}
+        placeholder={placeholder}
+        className={className}
+        autoFocus={autoFocus}
+        ref={ref}
+        {...rest}
+      />
+    );
+  }
+);
