460acac8d20fa9b893d5b9f7bd99003a0dbd6493
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-25 19:32:17 -0600

    Add navigation interfaces

---
+++ client/src/interfaces/navbar/navigation.ts
@@ -0,0 +1,11 @@
+export interface NavigationsItem {
+  name: string;
+  href: string;
+  icon: React.ComponentType | string;
+}
+
+export interface MainNavigation {
+  name: string;
+  href: string;
+  icon: React.ComponentType;
+}
