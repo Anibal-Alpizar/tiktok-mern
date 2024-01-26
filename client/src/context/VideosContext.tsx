c6cfda6233516824941198832c77ab962f383d7d
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-25 22:19:06 -0600

    Refactor VideosContext import and type definition

--- client/src/context/VideosContext.tsx
+++ client/src/context/VideosContext.tsx
@@ -1,12 +1,6 @@
 import { createContext } from "react";
-import { file, video } from "../interfaces/video/videoProps";
+import { videosContextProps } from "../interfaces/context/video/videosContextProps";
 
-interface VideosContextProps {
-  uploadVideo: (file: file, description: string) => void;
-  loadVideos: () => void;
-  videos: video[];
-}
-
-export const VideosContext = createContext<VideosContextProps | undefined>(
+export const VideosContext = createContext<videosContextProps | undefined>(
   undefined
 );
