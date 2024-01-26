c2f234987803b1a894241c36afd4e3b8e36e4019
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-25 22:16:17 -0600

    Update video interface names

--- client/src/context/provider/VideosProvider.tsx
+++ client/src/context/provider/VideosProvider.tsx
@@ -1,15 +1,15 @@
 import { VideosContext } from "../VideosContext";
 import { getVideosRequest, uploadVideoRequest } from "../../api/video.api";
-import { File, Video } from "../../interfaces/video";
+import { file, video } from "../../interfaces/video/videoProps";
 import { useState } from "react";
 
 export const VideoProvider = ({ children }: any) => {
-  const [videos, setVideos] = useState<Video[]>([]);
+  const [videos, setVideos] = useState<video[]>([]);
 
   const loadVideos = async () => {
     try {
       const response = await getVideosRequest();
-      const modifiedVideos = response.data.map((video: Video) => ({
+      const modifiedVideos = response.data.map((video: video) => ({
         ...video,
         tempFilePath: video.tempFilePath.replace("videos\\", ""),
       }));
@@ -20,7 +20,7 @@ export const VideoProvider = ({ children }: any) => {
     }
   };
 
-  const uploadVideo = async (file: File, description: string) => {
+  const uploadVideo = async (file: file, description: string) => {
     try {
       const formData = new FormData();
 
