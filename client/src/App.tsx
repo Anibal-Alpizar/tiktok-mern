2b0dc483b8f3e170e2b70f69da40db725c47b4d2
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-26 19:12:00 -0600

    Add AuthProvider to App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,17 @@ import Navbar from "./components/navbar/Navbar";
 import VideosPage from "./pages/videos/VideosPage";
 import UploadVideoPage from "./pages/videos/UploadVideoPage";
 import { VideoProvider } from "./context/provider/VideosProvider";
+import { AuthProvider } from "./context/provider/AuthProvider";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  // ! Fix this error: This JSX tag's 'children' prop expects type 'ReactNode[]' which requires multiple children, but only a single child was provided.
+  // ! Not Failed to compile
   return (
     <>
-      <Navbar />
+      <AuthProvider>
+        <Navbar />
+      </AuthProvider>
       <Routes>
         <Route
           element={
