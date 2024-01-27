8b6ba1f12bcd15682fb53d684ef95326ed22ec0e
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-26 19:12:08 -0600

    Add form validation and error handling to LoginCard component

--- client/src/components/login/LoginCard.tsx
+++ client/src/components/login/LoginCard.tsx
@@ -1,19 +1,49 @@
+import { useAuth } from "../../hooks/useAuth";
 import { loginCardProps } from "../../interfaces/login/loginCardProps";
 import { Input, Button, Label } from "../ui";
+import { useForm } from "react-hook-form";
 
 const LoginCard = ({ onClose }: loginCardProps) => {
+  const { handleSubmit, register } = useForm();
+  const { signIn, errors: loginErrors } = useAuth();
+
+  const onSubmit = handleSubmit(async (data) => {
+    await signIn(data);
+  });
+
   return (
     <div className="text-black fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded-2xl shadow-lg z-50">
+      {loginErrors && (
+        <div className="text-red-500 mb-4">
+          {Array.isArray(loginErrors) ? (
+            <ul>
+              {loginErrors.map((error, index) => (
+                <li key={index}>{error}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>{loginErrors.error}</p>
+          )}
+        </div>
+      )}
       <h2 className="mb-4">Login</h2>
-      <form>
+      <form onSubmit={onSubmit}>
         <Label htmlFor="username">
-          Usuario:
-          <Input type="text" />
+          Email:
+          <Input
+            className="mb-4 mt-2 w-full text-white bg-gray-800"
+            type="text"
+            {...register("email", { required: true })}
+          />
         </Label>
         <br />
         <Label htmlFor="password">
-          Contraseña:
-          <Input type="password" />
+          Password:
+          <Input
+            className="mb-4 mt-2 w-full text-white bg-gray-800"
+            type="password"
+            {...register("password", { required: true })}
+          />
         </Label>
         <br />
         <Button type="submit">Iniciar sesión</Button>
