b6ce034b260030b6af03999e89a476574356d7f4
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-26 19:12:19 -0600

    Add signIn functionality to AuthProvider

--- client/src/context/provider/AuthProvider.tsx
+++ client/src/context/provider/AuthProvider.tsx
@@ -1,5 +1,39 @@
 import { AuthContext } from "../AuthContext";
+import { loginRequest } from "../../api/auth.api";
+import Cookies from "js-cookie";
+import { useState } from "react";
 
-export const AuthProvider = ({ children }: any) => {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+export const AuthProvider = ({ children }: { children: React.ReactNode[] }) => {
+  const [user, setUser] = useState<any>(null);
+  const [isAuth, setIsAuth] = useState<boolean>(false);
+  const [errors, setErrors] = useState<any>(null);
+
+  const signIn = async (data: any) => {
+    try {
+      const res = await loginRequest(data);
+      console.log(res);
+      setUser(res.data.message);
+      setIsAuth(true);
+      console.log(`Login successful!`);
+      return res.data.message;
+    } catch (error: any) {
+      console.log(error.response.data);
+      if (Array.isArray(error.response.data))
+        return setErrors(error.response.data);
+      else setErrors(error.response.data);
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isAuth,
+        errors,
+        signIn,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
